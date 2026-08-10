69faf6f9d842ed25ad0419499626f39df6dbc1be
Matias <saafigueroam@gmail.com>
2026-08-10 17:18:40 -0300

    Ejercicios de la clase 2
    Programa que nos dice el año, programa que nos dice si el años es bisiesto o no y programa de resolución de ecuaciones cuadráticas

---
+++ Clase1/AnioBisiesto.java
@@ -0,0 +1,23 @@
+package Ejercicio3;
+import java.util.Scanner;
+
+public class AnioBisiesto {
+    public static boolean esBisiesto(int anio) {
+        return (anio % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un año: ");
+        int anio = sc.nextInt();
+
+        if (esBisiesto(anio)) {
+            System.out.println("El año " + anio + " es bisiesto.");
+        } else {
+            System.out.println("El año " + anio + " no es bisiesto.");
+        }
+
+        sc.close();
+    }
+}
