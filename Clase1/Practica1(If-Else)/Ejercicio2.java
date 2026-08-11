42866c256925b432e42d5675d5fb9ae902c91076
Santiago <144866430+Elcano03@users.noreply.github.com>
2026-08-11 18:58:01 -0300

    ejercicio1 clase 2

---
+++ Clase1/Practica1(If-Else)/Ejercicio2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void mostrarMes(int numeroMes) {
+        String[] nombres = {
+                "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+        };
+
+        int[] dias = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        if (numeroMes >= 1 && numeroMes <= 12) {
+            System.out.println("El mes " + numeroMes + " es " + nombres[numeroMes - 1]
+                    + " y tiene " + dias[numeroMes - 1] + " días.");
+        } else {
+            System.out.println("Número de mes inválido. Debe estar entre 1 y 12.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese el número de mes (1-12): ");
+        int mes = sc.nextInt();
+
+        mostrarMes(mes);
+
+        sc.close();
+    }
+}
---
+++ Clase1/Practica2(While)/Ejercicio1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ejercicio1{
+    public static double CalcularSuma(int x){
+        int i=1;
+        double suma=0.0;
+        while(i <= x){
+            suma += 1.0 /i;
+            i++;
+        }
+        return suma;
+    }
+    public static void main(String[] args) {
+        Scanner ingreso = new Scanner(System.in);
+        System.out.println("Ingrese un numero entero positivo:");
+        int x = ingreso.nextInt();
+
+        if (x>0){
+            double resultado = CalcularSuma(x);
+            System.out.println("El resultado es: "+resultado);
+        }else {
+            System.out.println("El numero ingresado no es un numero entero positivo");
+        }
+        ingreso.close();
+    }
+}
